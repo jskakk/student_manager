749ee63d71a3f605b5e3d9a16aea128d37f720ec
jskakk <basejascha@gmail.com>
2025-09-22 22:31:10 +0800

    feature(student_manager): setup static files

--- student_manager/views.py
+++ student_manager/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Student
 
 # Create your views here.
+
+
+
+def student_list(request):
+    students = Student.objects.all()
+    return render(request, 'student_manager/student_list.html', {'students': students})
+
+
+def student_detail(request, student_id):
+    student = get_object_or_404(Student, id=student_id)
+    return render(request, 'student_manager/student_detail.html', {'student': student})
